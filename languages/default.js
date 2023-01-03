8a1aac912c95453ddcc453041809f8bfc028f1f4
janlindso <jan.frode.lindso@gmail.com>
2023-01-03 15:31:40 +0100

    Upload

---
+++ languages/default.js
@@ -0,0 +1,53 @@
+﻿MyDefaultLanguage = {
+    "ActiveLanguages": {
+        "labelLanguageNN": [ "nn-NO", "Nynorsk" ],
+        "labelLanguageNB": ["nb-NO", "Norsk"]
+    },
+    "ObjNonLanguageElements": [
+    ],
+    "ObjDefaultElements": {
+        "nb-NO": [
+            { "labelForbruk": [ "id", "text", "Forbruk" ] },
+            { "labelTid": [ "id", "text", "Tid" ] },
+            { "labelVinkel": [ "id", "text", "Vinkel" ] },
+            { "AreaLabel": [ "class", "html", "Areal i m&sup2;" ] },
+            { "EffectLabel": [ "class", "text", "Virkningsgrad" ] },
+            { "ResultLabel": [ "class", "text", "Produksjon" ] },
+            { "cmdShowInstructions": [ "id", "text", "Instruksjoner" ] },
+            { "EffectLabel": [ "class", "text", "Virkningsgrad" ] },
+            { "ItemList1": [ "class", "text", "Høyttaler" ] },
+            { "ItemList2": [ "class", "text", "Klokkeradio" ] },
+            { "ItemList3": [ "class", "text", "Sparepære" ] },
+            { "ItemList4": [ "class", "text", "TV-skjerm" ] },
+            { "ItemList5": [ "class", "text", "Laptop" ] },
+            { "labelInstructionsContent": [ "class", "html", "<p>Klikk på pilene øverst til høyre for å endre solvinkel og klokkeslett.</p><p>I feltet for \"Produksjon\" kan du lese av effekten solpanelet leverer.</p><p>Ved å slå av og på de elektriske apparatene i displayet \"Forbruk\", kan du se hvor stor effekt apparatene drar til sammen.</p><p>Produksjonsdisplayet viser hvilken effekt solpanelet leverer. Det lyser grønt hvis det leverer nok effekt til alle apparat som er slått på, og rødt hvis effekten er for lav.</p><p>Nederst til høyre har du to parametere du kan endre på - solpanelets areal og virkningsgrad.</p><p>Disse er i utgangspunktet satt til 1 kvadratmeter og 16 prosent.</p>" ] },
+            { "labelInstructionsTitle": [ "class", "text", "Effekt av solceller" ] },
+            { "labelInstructionsClose": [ "class", "text", "Lukk" ] }
+            
+        ],
+        "nn-NO": [
+            { "labelForbruk": [ "id", "text", "Forbruk" ] },
+            { "labelTid": [ "id", "text", "Tid" ] },
+            { "labelVinkel": [ "id", "text", "Vinkel" ] },
+            { "AreaLabel": [ "class", "html", "Areal i m&sup2;" ] },
+            { "EffectLabel": [ "class", "text", "Virkningsgrad" ] },
+            { "ResultLabel": [ "class", "text", "Produksjon" ] },
+            { "cmdShowInstructions": [ "id", "text", "Instruksjoner" ] },
+            { "EffectLabel": [ "class", "text", "Virkningsgrad" ] },
+            { "ItemList1": [ "class", "text", "Høyttaler" ] },
+            { "ItemList2": [ "class", "text", "Klokkeradio" ] },
+            { "ItemList3": [ "class", "text", "Sparepære" ] },
+            { "ItemList4": [ "class", "text", "TV-skjerm" ] },
+            { "ItemList5": [ "class", "text", "Laptop" ] },
+            { "labelInstructionsContent": [ "class", "html", "<p>Klikk på pilene øvst til høgre for å endre solvinkel og klokkeslett.</p><p>I feltet for \“Produksjon\” kan du lese av effekten solpanelet leverer.</p><p>Ved å slå av og på dei elektriske apparata i displayet \“Forbruk\”, kan du sjå kor stor effekt apparata dreg til saman.</p><p>Produksjonsdisplayet viser hvilken effekt solpanelet leverer. Det lyse grønt viss det klarer å levere nok effekt til alle apparata som er på, og raudt viss forbruket er for stort i forhold til det solpanelet klarer å levere.</p><p>Nedst til høgre har du to parametrar du kan endre på – arealet på solpanelet og verknadsgraden.</p><p>Desse er i utgangspunktet sett til 1 kvadratmeter og 16 prosent.</p>" ] },
+            { "labelInstructionsTitle": [ "class", "text", "Instruksjoner" ] },
+            { "labelInstructionsClose": [ "class", "text", "Lukk" ] }
+        ]
+    },
+    "SingleDefaultElements": {
+        "nb-NO": {
+        },
+        "nn-NO": {
+        }
+    }
+};
